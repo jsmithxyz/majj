6ca172a3f9cf43286ab31a6a2085b6d0bdc39531
jsmithxyz <jordansmith3813@gmail.com>
2020-04-26 12:24:40 -0400

    fixed index bug

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
