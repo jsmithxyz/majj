e22618051a6f1daf6019cdd267de64814e530819
jpm <jpmckenna90@gmail.com>
2020-04-26 18:46:57 -0400

    Pointing app.get to client/public/index.html instead of client/build/index.html for troubleshooting purposes

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/majj", {
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 app.listen(PORT, function () {
