7803d2b38cc73f77478855ff82dfab1134520811
jpm <jpmckenna90@gmail.com>
2020-04-26 17:07:46 -0400

    finalized initial DB model config, seeding, and inclusion in server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -9,6 +10,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Mongodb connection - will need to set MONGODB_URI into .env when time comes
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/majj", {
+  useNewUrlParser: true,
+});
 
 // Send every request to the React app
 // Define any API routes before this runs
