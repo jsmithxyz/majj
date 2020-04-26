217ab7a3588ca6118c88d7eb15b87cee84b871f4
jpm <jpmckenna90@gmail.com>
2020-04-26 18:20:59 -0400

    Fixed index.html in client/build

--- client/build/precache-manifest.684e5b84713891cd8059847fc5b50d72.js
+++ client/build/precache-manifest.684e5b84713891cd8059847fc5b50d72.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9a99775e01b7a81fdeb8bca153478903",
+    "revision": "0fde5431db7f6fef4bb8b8569c0f5b6e",
     "url": "/index.html"
   },
   {
-    "revision": "d6d98dd9e26c3cd21493",
+    "revision": "43cd8324def1a328c1e0",
     "url": "/static/css/2.69121389.chunk.css"
   },
   {
-    "revision": "5b2d3e1b396106b55e35",
-    "url": "/static/css/main.448ea188.chunk.css"
+    "revision": "a2ebdba7bf36f84db5d2",
+    "url": "/static/css/main.a97109aa.chunk.css"
   },
   {
-    "revision": "d6d98dd9e26c3cd21493",
-    "url": "/static/js/2.c41292b5.chunk.js"
+    "revision": "43cd8324def1a328c1e0",
+    "url": "/static/js/2.703f97e0.chunk.js"
   },
   {
     "revision": "005c97f82cee60e20eac659314ea29ec",
-    "url": "/static/js/2.c41292b5.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.703f97e0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5b2d3e1b396106b55e35",
-    "url": "/static/js/main.4dde9412.chunk.js"
+    "revision": "a2ebdba7bf36f84db5d2",
+    "url": "/static/js/main.9d67f144.chunk.js"
   },
   {
     "revision": "dfd1bb87e6756a0869c2",
