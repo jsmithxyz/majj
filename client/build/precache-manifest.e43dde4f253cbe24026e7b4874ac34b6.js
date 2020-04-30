885b575a852e52908095f92ba71747c580744470
Max <55365382+maxkulicke@users.noreply.github.com>
2020-04-30 16:00:59 -0400

    Max branch (#45)
    * ton of progress on state reactive gem cards
    
    * further refined the API debugging
    
    * pushing up mostly debugged, need help
    
    * fixed async issue! wrapped api call in Promise.all(), which solved it, removed unnecessary awaits from mine.js loadItems(). also, added knuth shuffle function to jumble results
    
    * added back necessary closure to have mine.js function
    
    * cleaned up, still trying to debug the double firing
    
    * committing update on GlobalState.js
    
    * committing to pull down
    
    * added queue making functionality, overhauled filter state, created mutateFilter, updated API call, checkbox handler logic
    
    * removed some console logs

--- client/build/precache-manifest.e43dde4f253cbe24026e7b4874ac34b6.js
+++ client/build/precache-manifest.e43dde4f253cbe24026e7b4874ac34b6.js
@@ -1,29 +1,5 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-<<<<<<< HEAD:client/build/precache-manifest.1ba3aebd65678f9c2f5b699926137e8b.js
-    "revision": "5db8d1bdfcf1f7e5ff30089fbfa4fbf3",
-    "url": "/index.html"
-  },
-  {
-    "revision": "875c8ef8f64664c799c6",
-    "url": "/static/css/2.69121389.chunk.css"
-  },
-  {
-    "revision": "80b123c4fdf1e2712083",
-    "url": "/static/css/main.7d58ea4d.chunk.css"
-  },
-  {
-    "revision": "875c8ef8f64664c799c6",
-    "url": "/static/js/2.7a65acc5.chunk.js"
-  },
-  {
-    "revision": "005c97f82cee60e20eac659314ea29ec",
-    "url": "/static/js/2.7a65acc5.chunk.js.LICENSE.txt"
-  },
-  {
-    "revision": "80b123c4fdf1e2712083",
-    "url": "/static/js/main.4eead891.chunk.js"
-=======
     "revision": "fa7ebaa594acc5febd4f7e7b33004132",
     "url": "/index.html"
   },
@@ -46,7 +22,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "5ad826185e0bb8c50dad",
     "url": "/static/js/main.c0af02ed.chunk.js"
->>>>>>> 6500d22afaaf5a7a4b16ec22d84b267c03b23fe4:client/build/precache-manifest.e43dde4f253cbe24026e7b4874ac34b6.js
   },
   {
     "revision": "dfd1bb87e6756a0869c2",
