4c645205bc5084ecea15ae8b1dfccce325ce7a33
Max Kulicke <mkulicke@gmail.com>
2020-04-30 20:04:11 -0400

    fixed card issue / binding mutateFilter w/ filter resolved

--- client/src/pages/Mine.js
+++ client/src/pages/Mine.js
@@ -14,7 +14,10 @@ function Mine() {
   const [state, dispatch] = useStoreContext();
   const { filter, queue, items } = state;
 
-  const [tempItems, setTempItems] = useState(sampleItems);
+  let newGems = itemizer(items)
+  const [gems, setGems] = useState(newGems);
+  // const [currentItems, setCurrentItems] = useState(items);
+
 
   let flexbox = {
     display: "flex",
@@ -29,6 +32,11 @@ function Mine() {
   useEffect(() => {
     // console.log(queue);
   }, [queue]);
+
+  useEffect(() => {
+    let newGems = itemizer(items)
+    setGems(newGems);
+  }, [items]);
 
   function createQueue() {
     const newQueue = [];
@@ -74,32 +82,25 @@ function Mine() {
     return array;
   }
 
-  //would like to dry this up
-  if (items) {
-    return (
-      <div>
-        <MainNav />
-        <div className="flexbox-containter" style={flexbox}>
-          <LeftNav />
-          {Object.keys(items).map((key) => (
-            <Gems key={key} details={items[key]} />
-          ))}
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <MainNav />
-        <div className="flexbox-containter" style={flexbox}>
-          <LeftNav />
-          {Object.keys(tempItems).map((key) => (
-            <Gems key={key} details={tempItems[key]} />
-          ))}
-        </div>
-      </div>
-    );
+  function itemizer(items) {
+    let newGems = Object.keys(items).map((key) => (
+      <Gems key={key} details={items[key]} />))
+    return newGems;
   }
+
+  //would like to dry this up
+  // if (items) {
+  return (
+    <div>
+      <MainNav />
+      <div className="flexbox-containter" style={flexbox}>
+        <LeftNav />
+
+        {gems}
+
+      </div>
+    </div>
+  );
 }
 
 export default Mine;
