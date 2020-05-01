a293b06fd09f28712f5491898e3d253ad173665e
Alyssa Cucunato <acucunato18@gmail.com>
2020-05-01 14:28:01 -0400

    main nav scroll taken out

--- client/src/components/MainNav/MainNav.js
+++ client/src/components/MainNav/MainNav.js
@@ -1,36 +1,13 @@
 import React from "react";
-import { useState } from "react";
-import useWindowScrollPosition from "@rehooks/window-scroll-position";
 import "./MainNav.css";
 import purplegem from "../../img/purplegem.png";
 import { Navbar } from "react-bootstrap";
 
 function MainNav() {
-  // change to nav on scroll, need to finish figuring this out
-  const [change, setChange] = useState(false);
-  const changePosition = 0;
-
-  let position = useWindowScrollPosition();
-  // position == { x: 0, y: 0 }
-
-  if (position.y > changePosition && !change) {
-    setChange(true);
-  }
-
-  if (position.y <= changePosition && change) {
-    setChange(false);
-  }
-
-  let style = {
-    backgroundColor: change ? "transparent" : "#7e7e7e",
-    transition: "400ms ease",
-  };
-
   return (
     <Navbar
       sticky="top"
       className="justify-content-center text-center container-fluid majj"
-      style={style}
     >
       <div>
         <div className="animated flip majj-text">
