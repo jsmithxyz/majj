316c904235600755932151c13151b92508a3dc00
jsmithxyz <jordansmith3813@gmail.com>
2020-05-02 10:30:46 -0400

    fixed API issue

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -16,21 +16,17 @@ export default {
           if (filter[key] === true) {
             let queryURL = `https://microsoft-azure-bing-news-search-v1.p.rapidapi.com/search?q=${key}&count=9&sortby=dat`;
 
-        if (filter[key] === true) {
-
-          let queryURL = `https://microsoft-azure-bing-news-search-v1.p.rapidapi.com/search?q=${key}&count=9&sortby=dat`;
-
-          let config = {
-            method: "get",
-            url: queryURL,
-            headers: {
-              "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
-            },
-          };
-          let axiosReturn = await axios(config);
-          return axiosReturn;
-        }
-      })
+            let config = {
+              method: "get",
+              url: queryURL,
+              headers: {
+                "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+              },
+            };
+            let axiosReturn = await axios(config);
+            return axiosReturn;
+          }
+        })
       );
 
       const filteredResults = resArr.filter((result) => result !== undefined);
