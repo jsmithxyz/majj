0c625742439dccb466182aaa9c32dbda300d4e4e
Max Kulicke <mkulicke@gmail.com>
2020-05-01 20:05:50 -0400

    same commit message as previous commit, pushing to fix conflicts

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -13,17 +13,21 @@ export default {
     if (filterNotEmpty) {
       let resArr = await Promise.all(Object.keys(filter).map(async (key) => {
 
-            let config = {
-              method: "get",
-              url: queryURL,
-              headers: {
-                "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
-              },
-            };
-            let axiosReturn = await axios(config);
-            return axiosReturn;
-          }
-        })
+        if (filter[key] === true) {
+
+          let queryURL = `https://microsoft-azure-bing-news-search-v1.p.rapidapi.com/search?q=${key}&count=9&sortby=dat`;
+
+          let config = {
+            method: "get",
+            url: queryURL,
+            headers: {
+              "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+            },
+          };
+          let axiosReturn = await axios(config);
+          return axiosReturn;
+        }
+      })
       );
 
       const filteredResults = resArr.filter((result) => result != undefined);
