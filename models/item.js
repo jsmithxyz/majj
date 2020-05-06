e74d9c188378665fc032777d9e120091f8131b25
jpm <jpmckenna90@gmail.com>
2020-05-06 19:53:48 -0400

    Cleaned up unnecessary comments and console.logs. Kept comments that will be relevant to future development intact.

--- models/item.js
+++ models/item.js
@@ -1,12 +1,9 @@
-// goes in /models
-
-
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
   title: { type: String, required: true },
-  url: { type: String, required: true }
+  url: { type: String, required: true },
 });
 
 const Item = mongoose.model("Item", itemSchema);
